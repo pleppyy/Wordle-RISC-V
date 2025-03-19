a1451b86a99b9f70bd0148db74d5734d69247cbf
June Lin <91984999+pleppyy@users.noreply.github.com>
2025-03-18 23:49:14 -0400

    Create alphabet_coordinates.h

---
+++ header_files/alphabet_coordinates.h
@@ -0,0 +1,61 @@
+#ifndef ALPHABET_COORDINATES_H
+#define ALPHABET_COORDINATES_H
+
+const int x_a = 40;
+const int x_b = 61;
+const int x_c = 82;
+const int x_d = 102;
+const int x_e = 40;
+const int x_f = 61;
+const int x_g = 82;
+const int x_h = 102;
+const int x_i = 40;
+const int x_j = 61;
+const int x_k = 82;
+const int x_l = 102;
+const int x_m = 40;
+const int x_n = 61;
+const int x_o = 82;
+const int x_p = 102;
+const int x_q = 40;
+const int x_r = 61;
+const int x_s = 82;
+const int x_t = 102;
+const int x_u = 40;
+const int x_v = 61;
+const int x_w = 82;
+const int x_x = 102;
+const int x_y = 61;
+const int x_z = 82;
+
+const int y_a = 50;
+const int y_b = 50;
+const int y_c = 50;
+const int y_d = 50;
+const int y_e = 71;
+const int y_f = 71;
+const int y_g = 71;
+const int y_h = 71;
+const int y_i = 92;
+const int y_j = 92;
+const int y_k = 92;
+const int y_l = 92;
+const int y_m = 113;
+const int y_n = 113;
+const int y_o = 113;
+const int y_p = 113;
+const int y_q = 134;
+const int y_r = 134;
+const int y_s = 134;
+const int y_t = 134;
+const int y_u = 155;
+const int y_v = 155;
+const int y_w = 155;
+const int y_x = 155;
+const int y_y = 176;
+const int y_z = 176;
+
+const int alphabet_coordinates[2][26] = {{x_a, x_b, x_c, x_d, x_e, x_f, x_g, x_h, x_i, x_j, x_k, x_l, x_m, x_n, x_o, x_p, x_q, x_r, x_s, x_t, x_u, x_v, x_w, x_x, x_y, x_z},
+                                         {y_a, y_b, y_c, y_d, y_e, y_f, y_g, y_h, y_i, y_j, y_k, y_l, y_m, y_n, y_o, y_p, y_q, y_r, y_s, y_t, y_u, y_v, y_w, y_x, y_y, y_z}};
+
+#endif
