90421575ea392ba702f3f3b187198a97f277078c
June Lin <91984999+pleppyy@users.noreply.github.com>
2025-03-18 23:48:07 -0400

    Create board_coordinates.h

---
+++ header_files/board_coordinates.h
@@ -0,0 +1,20 @@
+#ifndef BOARD_COORDINATES_H
+#define BOARD_COORDINATES_H
+
+const int x_1 = 167;
+const int x_2 = 190;
+const int x_3 = 213;
+const int x_4 = 236;
+const int x_5 = 259;
+
+const int y_1 = 50;
+const int y_2 = 73;
+const int y_3 = 96;
+const int y_4 = 119;
+const int y_5 = 143;
+const int y_6 = 166;
+
+const int x_coords[5] = {x_1, x_2, x_3, x_4, x_5};
+const int y_coords[6] = {y_1, y_2, y_3, y_4, y_5, y_6};
+
+#endif
